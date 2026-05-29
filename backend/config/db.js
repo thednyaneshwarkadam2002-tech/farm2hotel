f07b346a37b54913d47c8accdc5b088dc05e7d4b
dnyaneshwar <dnyaneshwarkadam7498@gmail.com>
2026-05-29 12:16:30 +0530

    fixed railway mysql connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,19 @@
-
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-   database: "farm2hotel_db",
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
-
     if (err) {
-        console.log("Database Error ❌");
-        console.log(err);
-        return;
+        console.error("Database connection failed:", err);
+    } else {
+        console.log("MySQL Connected Successfully");
     }
-
-    console.log("Database Connected ✅");
 });
 
 module.exports = db;

5c5aa104ea426e41a5bd2472af23faec2f5c0b5a
dnyaneshwar <dnyaneshwarkadam7498@gmail.com>
2026-05-29 12:57:32 +0530

    fixed railway mysql env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,20 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-host: process.env.DB_HOST,
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
-port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
-
-if (err) {
-console.log("Database connection failed:", err);
-return;
-}
-
-console.log("MySQL Connected Successfully");
-
+  if (err) {
+    console.log("Database Error ❌");
+    console.log(err);
+  } else {
+    console.log("MySQL Connected Successfully ✅");
+  }
 });
 
 module.exports = db;
